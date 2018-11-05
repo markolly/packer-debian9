124e0b82886480dbf891d4dbe308e0b020dcc6b8
markolly <mark@oliver.cx>
2018-11-05 10:44:08 +0000

    Removed unnecessary packages to purge from cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,7 +3,7 @@
 printf "STEP: Remove unused packages\n"
 # Uninstall Puppet and remove PPA.
 apt -y remove --purge puppet
-apt -y remove --purge ppp pppconfig pppoeconf cpp gcc g++ libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6 linux-source
+apt -y remove --purge cpp gcc libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
 apt -y remove --purge '.*-dev$'
 apt -y autoremove
 
@@ -11,23 +11,23 @@ apt -y autoremove
 printf "STEP: Remove APT cache\n"
 apt-get clean -y
 apt-get autoclean -y
- 
+
 # Remove APT files
 printf "STEP: Zero free space to aid VM compression\n"
 find /var/lib/apt -type f | xargs rm -f
- 
+
 # Remove documentation files
 printf "STEP: Remove documentation files\n"
 find /var/lib/doc -type f | xargs rm -f
- 
+
 # vagrant: Remove Virtualbox specific files
 printf "STEP: vagrant: Remove Virtualbox specific files\n"
 rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
- 
+
 # Remove Linux headers
 printf "STEP: Remove Linux headers\n"
 rm -rf /usr/src/linux-headers*
- 
+
 # Remove bash history
 printf "STEP: Remove bash history\n"
 unset HISTFILE
@@ -39,11 +39,11 @@ rm -f /home/vagrant/*.sh
 # vagrant: Remove bash history
 printf "STEP: vagrant: Remove bash history\n"
 rm -f /home/vagrant/.bash_history
- 
+
 # Cleanup log files
 printf "STEP: Cleanup log files\n"
 find /var/log -type f | while read f; do echo -ne '' > $f; done;
- 
+
  # Zero free space to aid VM compression
 printf "STEP: Zero free space to aid VM compression\n"
 dd if=/dev/zero of=/EMPTY bs=1M
@@ -55,15 +55,15 @@ count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`;
 count=$((count -= 1))
 dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
 rm /tmp/whitespace;
- 
+
 # Whiteout /boot
 printf "STEP: Whiteout /boot\n"
 count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
 count=$((count -= 1))
 dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
 rm /boot/whitespace;
- 
-# Whiteout swap 
+
+# Whiteout swap
 printf "STEP: Whiteout swap\n"
 swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
 swapoff $swappart;
