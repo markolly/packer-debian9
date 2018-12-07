401ca381b9e5188a2d3da511323ebbc2b5043e1e
Mark Oliver <markolly@users.noreply.github.com>
2018-12-07 16:32:56 +0000

    Added puppet6 comments to puppet installation script

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -3,8 +3,11 @@
 # Install Puppet repository.
 #apt -y update && apt-get -y upgrade
 #apt -y install wget
-#wget https://apt.puppetlabs.com/puppet-release-xenial.deb
-#sudo dpkg -i puppet-release-xenial.deb
+#wget https://apt.puppetlabs.com/puppet6-release-stretch.deb
+#dpkg -i puppet6-release-stretch.deb
+#apt -y update
+#apt -y install puppet-agent
+#ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
 
 # Install Puppet.
 apt -y update && apt -y upgrade
